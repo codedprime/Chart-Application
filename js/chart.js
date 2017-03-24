c7bc758b13476c4978ff527e52d7249a1c2ed402
Obinna Okwuolisa <kingobiorah@gmail.com>
2017-03-24 02:41:13 +0100

    improves the user interface

--- js/chart.js
+++ js/chart.js
@@ -14,25 +14,32 @@ function reload() {
     eachLabel = 0;
 
    var canvas = document.getElementById('myCanvas');
-            var ctx = canvas.getContext('2d'); ctx.clearRect(0,0,canvas.width,canvas.height);
-             ctx.beginPath();
+   var ctx = canvas.getContext('2d'); ctx.clearRect(0,0,canvas.width,canvas.height);
+   ctx.beginPath();
 }
 
 
 
-function data()
-{        
-           
-            eachData = document.getElementById('data').value;
-            eachLabel = document.getElementById('label').value;
-            eachColor = document.getElementById('color').value;
-            console.log(eachData);
-            parseInt(eachData);
-            myData.push(eachData);
-            myLabel.push(eachLabel);
-            myColor.push(eachColor);
+function data() {   
+    eachData = document.getElementById('data').value;
+    eachLabel = document.getElementById('label').value;
+    eachColor = document.getElementById('color').value;
+
+    parseInt(eachData);
+
+    if (eachData < 0) {
+        document.getElementById('show').innerHTML = "Invalid Value!!!";
+        reload();
+    }else if(eachData>0){
+      document.getElementById('show').innerHTML = eachData +" Added for analysis";
+    }
+    
+    myData.push(eachData);
+    myLabel.push(eachLabel);
+    myColor.push(eachColor);
  
 }
+
 
 function barChart() {       
     var canvas = document.getElementById('myCanvas');
@@ -46,7 +53,10 @@ function barChart() {
     
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.beginPath();  
+    ctx.beginPath(); 
+    ctx.fillStyle = 'white'; 
+    ctx.fillRect(0, 0, canvas.width, canvas.height);        
+    
 
     if (myData.length >5) {
         width = (canvas.width/myData.length)
@@ -60,8 +70,8 @@ function barChart() {
         }
     }
 
-    ctx.moveTo(0,0);
-    ctx.lineTo(0,480);
+    ctx.moveTo(7,0);
+    ctx.lineTo(7,480);
     ctx.lineTo(600,480);
     ctx.fillStyle ='black';
     ctx.stroke();
@@ -79,7 +89,7 @@ function barChart() {
     for (var i=0; i < myData.length; i++) {
         var y = myData[i];
         var y2 = (y/max)*450;
-        ctx.fillStyle ='green';               
+        ctx.fillStyle =myColor[i];               
         ctx.fillRect(x, 480 - y2, width, y2);        
         ctx.fillStyle = 'black';
         ctx.fillText(myData[i], x+x2, 480 - y2);
@@ -87,6 +97,8 @@ function barChart() {
         x += width + x2;
     }
 }
+
+
 function histogram() {       
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');     
@@ -95,11 +107,13 @@ function histogram() {
     var max = myData[0]; 
     var width  = 100;
     var x = width* 0.4;
-    var x2;
-    
+    var x2;    
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.beginPath();  
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);        
+
 
     if (myData.length >5) {
         width = (canvas.width/myData.length)
@@ -113,8 +127,8 @@ function histogram() {
         }
     }
 
-    ctx.moveTo(0,0);
-    ctx.lineTo(0,480);
+    ctx.moveTo(7,0);
+    ctx.lineTo(7,480);
     ctx.lineTo(600,480);
     ctx.fillStyle ='black';
     ctx.stroke();
@@ -132,7 +146,7 @@ function histogram() {
     for (var i=0; i < myData.length; i++) {
         var y = myData[i];
         var y2 = (y/max)*450;
-        ctx.fillStyle ='green';               
+        ctx.fillStyle =myColor[i];               
         ctx.fillRect(x, 480 - y2, width, y2);
         ctx.strokeRect(x, 480 - y2, width, y2);      
         ctx.fillStyle = 'black';
@@ -151,7 +165,9 @@ function pieChart(){
     var fraction, angle, midAngle, x, y, labelRadius;
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.beginPath();         
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);        
 
     
     for (var i in myData){
@@ -164,19 +180,19 @@ function pieChart(){
         angle = prevAngle + fraction*Math.PI*2;        
         ctx.fillStyle = myColor[i];
         ctx.beginPath();        
-        ctx.arc(200,200,150,prevAngle,angle,false);
-        ctx.lineTo(200,200);
+        ctx.arc(300,250,250,prevAngle,angle,false);
+        ctx.lineTo(300,250);
         ctx.fill();
         ctx.strokeStyle = 'black';
         ctx.stroke();
         midAngle = prevAngle + (angle - prevAngle) / 2;
         labelRadius = 150 * .75;
-        x = 200 + (labelRadius) * Math.cos(midAngle);
-        y = 200 + (labelRadius) * Math.sin(midAngle);
-        ctx.font = '24px serif';
+        x = 300 + (labelRadius) * Math.cos(midAngle);
+        y = 250 + (labelRadius) * Math.sin(midAngle);
+        ctx.font = '16px serif';
         ctx.fillStyle = 'black';
         ctx.fillText(myLabel[i], x, y);
-        ctx.fillText(Math.floor(fraction*360)+ '\xB0 ', x+10, y-20);        
+        ctx.fillText(Math.abs(Math.floor(fraction*360))+ '\xB0 ', x+10, y-20);        
         prevAngle = angle;
     }
 }  
@@ -188,13 +204,15 @@ function lineChart(){
     var x = 0;  
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.beginPath(); 
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);   
     ctx.font = '16px serif';
     ctx.fillStyle ='black';
-    ctx.moveTo(0,0);
-    ctx.lineTo(0,480);
+    ctx.moveTo(7,0);
+    ctx.lineTo(7,480);
     ctx.lineTo(600,480);           
-    ctx.moveTo(0,480);
+    ctx.moveTo(7,480);
 
     for (var i in myData) {
         if (myData[i] >= max) {
@@ -208,7 +226,8 @@ function lineChart(){
         y = myData[i];
         var y2 = (y/max)*450;        
         x += 50;
-        ctx.lineTo(x,480 - y2);
+        ctx.fillStyle = 'black';
+        ctx.lineTo(x,480 - y2);   
         ctx.fillStyle = 'black';
         ctx.fillText(myData[i], x ,480 - y2);
         ctx.fillText(myLabel[i], x+x2, 495);
